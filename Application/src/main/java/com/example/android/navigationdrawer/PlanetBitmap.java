8b191152be6d27ce24f97e69751bb37fb69385a8
Vivek Jain <vivekjain10@gmail.com>
2014-11-30 22:33:44 +0530

    Implement caching of bitmaps

--- Application/src/main/java/com/example/android/navigationdrawer/PlanetBitmap.java
+++ Application/src/main/java/com/example/android/navigationdrawer/PlanetBitmap.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class PlanetBitmap {
 
+  private static Map<String, Bitmap> cache = new HashMap<String, Bitmap>();
   private Activity activity;
 
   public PlanetBitmap(Activity activity) {
@@ -15,9 +18,19 @@ public class PlanetBitmap {
   }
 
   public Bitmap getBitmap(String planet) {
+    Bitmap bitmap = cache.get(planet);
+    if (bitmap != null) return bitmap;
+
     int imageId = activity.getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
         "drawable", activity.getPackageName());
 
-    return BitmapFactory.decodeResource(activity.getResources(), imageId);
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException ignored) {
+    }
+
+    bitmap = BitmapFactory.decodeResource(activity.getResources(), imageId);
+    cache.put(planet, bitmap);
+    return bitmap;
   }
 }
